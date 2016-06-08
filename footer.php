c6fdba1baf2cee4866fd02c10ece1eefff795c74
kmja <karl.mj.andersson@gmail.com>
2016-06-08 16:19:44 +0200

    footer skeleton

---
+++ footer.php
@@ -0,0 +1,58 @@
+<?php
+	//Footer template file
+?>
+
+<div class="footer-wrapper">
+
+	<!-- Footer content -->
+	<!-- Links -->
+	<div class="links-wrapper">
+		<!-- Instagram links -->
+		<p class="links-title">Instagram</p>
+		<ul class="links-instagram">
+			<li class="links-element">
+				<!-- Content -->
+			</li>
+		</ul>
+
+		<!-- Snapchat links -->
+		<p class="links-title">Snapchat</p>
+		<ul class="links-snapchat">
+			<li class="links-element">
+				<!-- Content -->
+			</li>
+		</ul>
+
+		<!-- Email links -->
+		<p class="links-title">Email</p>
+		<ul class="links-instagram">
+			<li class="links-element">
+				<!-- Content -->
+			</li>
+		</ul>
+	</div>
+
+	<!-- Credits -->
+	<div class="credits-wrapper">
+		<h3 class="credits-title">Creators (kom på något mer temaenligt namn)</h3>
+		<ul class="credits-list"> <!-- Alphabetic order -->
+			<li class="credits-item">
+				<h4>Karl Andersson</h4>
+				<p>Roll</p>  
+			</li>
+			<li class="credits-item">
+				<h4>Gustav Bergman</h4>
+				<p>Roll</p>  
+			</li>
+			<li class="credits-item">
+				<h4>Johanna Bromark</h4>
+				<p>Roll</p>  
+			</li>
+			<li class="credits-item">
+				<h4>Arvid Larsson</h4>
+				<p>Roll</p>  
+			</li>
+		</ul>
+	</div>
+
+</div>
--- mall.php
+++ mall.php
@@ -4,9 +4,9 @@
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width,initial-scale=1">
 		<title>Mottagningen 2016</title>
-		<link rel="stylesheet" href="main.css" type="text/css">
+		<link rel="stylesheet" href="css/main.css" type="text/css">
 		<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700|Pirata+One' rel='stylesheet' type='text/css'>
-		<script type="text/javascript" src="main.js"></script>
+		<script type="text/javascript" src="js/main.js"></script>
 	</head>
 
 	<body>
