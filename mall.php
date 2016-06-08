63f5421e2246798d045303b050b1d86469fd34cb
kmja <karl.mj.andersson@gmail.com>
2016-06-08 15:20:28 +0200

    utkast mall.php

---
+++ mall.php
@@ -0,0 +1,18 @@
+<?php 
+	//Load header
+	include header.php
+?>
+
+<div class="content-wrapper">
+
+	<!-- Content -->
+
+</div>
+
+<?php
+	//Load footer
+	include footer.php
+?>
+
+</body>
+</html>
