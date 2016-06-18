ccd8dec9e5e9e7e8062f6b914a5f0c9e10bef083
Gubergma <gubergma@gmail.com>
2016-06-18 15:52:02 +0200

    Uppdaterat filer, fixat databaskoppling mm

--- inc_header.php
+++ inc_header.php
@@ -3,29 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title>Mottagningen 2016</title>
     <link rel="stylesheet" href="css/main.css" type="text/css">
     <link rel="stylesheet" href="css/responsive.css" type="text/css">
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700|Pirata+One' rel='stylesheet' type='text/css'>
     <script type="text/javascript" src="js/main.js"></script>
-  </head>
-
-  <body>
-
-    <?php
-      //Load header
-      include ('top_content.php');
-    ?>
-
-    <div class="content-wrapper">
-      <!-- Content -->
-    </div>
-
-    <?php
-      //Load footer
-      include ('footer.php');
-    ?>
-
-  </body>
-
-</html>
--- index.php
+++ index.php
@@ -1,5 +1,25 @@
-<?php
+   <?php
+    //Load header
+    include ('inc_header.php');
+  ?>
+    <title>Mottagningen 2016</title>
+  </head>
 
-// woop :D
+  <body>
 
-?>
+    <?php
+      //Load top content
+      // include ('inc_top_content.php');
+    ?>
+
+    <div class="content-wrapper">
+      <!-- Content -->
+    </div>
+
+    <?php
+      //Load footer
+      include ('inc_footer.php');
+    ?>
+
+  </body>
+</html>
