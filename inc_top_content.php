2b73c9adafd5615f2fc90b09598cf4c1f3c24a24
Gubergma <gubergma@gmail.com>
2016-06-28 09:15:40 +0200

    Lite design och header

--- inc_top_content.php
+++ inc_top_content.php
@@ -1,13 +1,28 @@
+<?php
+$admin = $_SESSION['admin'];
+?>
+
 <div class="header">
-      <a href="/">Startsida </a>
-      <?php
-      $admin = $_SESSION['admin'];
-      if ($admin) {
-      ?><a href="adminpanel.php">Adminpanel</a><?php
-      }
-      ?>
-      <a href="logout.php">Logga ut </a>
+    <div class="header-wave-back"></div>
+	<div class="constrainer">
+		<div class="header-ship"></div>
+		<div class="header-logo"></div>
+		<div class="header-sun"></div>
+    </div>
 
-
-
+    <div class="header-wave"></div>
 </div>
+<nav class="menubar">
+	<div class="constrainer">
+		<ul class="sitenav">
+			<li><a href="/">Startsida </a></li>
+			<?php      
+			if ($admin) {
+			echo'<li><a href="adminpanel.php">Adminpanel</a></li>';
+	      	}
+	      	?>
+	      	<li><a href="logout.php">Logga ut </a></li>
+
+		</ul>
+	</div>
+</nav>
---
+++ lights/index.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>MTGN 2016</title>
+<meta name="viewport" content="width=320, initial-scale=1, user-scalable=no">
+<meta name="apple-mobile-web-app-status-bar-style" content="black"/>
+<meta content="yes" name="apple-mobile-web-app-capable"/>
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png"/>
+<link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+	<div id="main">
+		<div class="holder">
+       		<div class="holderImage">
+           	<img src="img/welcomeRed.png" width="200" height="200" />
+           </div>
+           <div class="holderHeadline">
+           Välkommen till medieteknik Hejhej
+           </div>
+           <div class="holderBread">
+Vi på sektionen för Medieteknik vill välkomna dig till programmet och tar tillfället i akt under din första tid på högskolan, nämligen under mottagningen. Mottagningen anordnas av äldre studenter på sektionen tillsammans med Tekniska Högskolans Studentkår (THS) och finns för att du ska få en så bra start på dina högskolestudier som möjligt. Om din tid på Medieteknik är en hel dag så är mottagningen helt enkelt din frukost. 
+<br /><br />
+På den här sidan kommer du så småningom finna information om mottagningens aktiveter, glädjas åt bilder, filmer och mycket mer skoj.
+<br />
+Ha en fortsatt trevlig sommar och vi ser fram emot att träffa dig i höst!
+<br /><br />
+Mottagningsansvariga,
+<br />
+Jonathan, Sara & Björn
+           </div>
+      	</div>
+       	<div class="holder">
+       		<div class="holderImage">
+           	<img src="img/schedule.png" width="200" height="200" />
+           </div>
+           <div class="holderHeadline">
+           Håll dig uppdaterad
+           </div>
+           <div class="holderBread">
+Självklart vill vi att du ska kunna vara med på så många mottagningsaktiviteter som möjligt. Därför får du redan nu tillgång till det <a href="https://www.google.com/calendar/embed?src=m2aof9kqs64nbj2l0ijsbss6io%40group.calendar.google.com&ctz=Europe/Stockholm" id="schema">preliminära schemat</a> för att kunna planera inför din start på kth. 
+<br /><br />
+Stödjer din kalender iCal kan du lägga till schemat direkt genom att klicka <a href="https://www.google.com/calendar/ical/m2aof9kqs64nbj2l0ijsbss6io%40group.calendar.google.com/public/basic.ics" id="schema">här</a>. Tänk på att alla datum inte är helt satta och kalendern kan komma att uppdateras.
+           </div>
+      	</div>
+       <div class="holder">
+       		<div class="holderImage">
+           	<img src="img/contact.png" width="200" height="200" />
+           </div>
+           <div class="holderHeadline">
+           Har du några frågor?
+           </div>
+           <div class="holderBread">
+Sitt inte där och undra! Vi svarar på alla frågor och funderingar du har som rör mottagningen och dess schema. 
+<br />
+Skicka ett mail till <a href="mailto:oph@medieteknik.com">oph@medieteknik.com</a> eller ring oss på telefonnummer.
+           </div>
+      	</div>
+       <div id="footer">
+       		<div id="footerHolder">
+            	<div id="logo">
+                	<a href="http://www.medieteknik.com/">
+              			<img src="img/logo.png" width="41" height="50" />
+                   	</a>
+              	</div>
+              	<div id="footerInfo">
+                  <div id="footerSocial">
+                  	<div class="socialObject">
+                    		<a href="#pint">
+                    		<img src="img/pint.png" width="13" height="13" class="socialObjectImage" />
+                         	</a>
+                    	</div>
+                    	<div class="socialObject">
+                    		<a href="#twitt">
+                    		<img src="img/twitt.png" width="13" height="13" class="socialObjectImage" />
+                         	</a>
+                      </div>
+                    	<div class="socialObject">
+                    		<a href="#insta">
+                    		<img src="img/insta.png" width="13" height="13" class="socialObjectImage" />
+                         	</a>
+                     	</div>
+                    	<div class="socialObject">
+                    		<a href="#face">
+                    		<img src="img/face.png" width="13" height="13" class="socialObjectImage" />
+                         	</a>
+                     	</div>
+                    	<div class="socialObject">
+                    		<a href="#google">
+                    		<img src="img/google.png" width="13" height="13" class="socialObjectImage" />
+                         	</a>
+                     	</div>
+						<div class="locLine"></div>
+                    	<div class="socialObject">
+                    		<a href="https://www.google.com/maps/place/Drottning+Kristinas+väg+7,+114+28+Stockholm,+Sverige/@59.3463109,18.0717019,114m/data=!3m2!1e3!4b1!4m2!3m1!1s0x465f9d41d6ee77fd:0x91e8fcabfcc4c7df">
+                    		<img src="img/location.png" width="13" height="13" class="socialObjectImage" />
+                         	</a>
+                     	</div>
+                  </div>
+              	</div>
+           </div>
+       </div>
+	</div>
+</body>
+</html>
