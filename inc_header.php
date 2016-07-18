a38661204986d62ae47dde739a471f6ae661c187
Gustav <gubergma@gmail.com>
2016-07-18 17:39:07 +0200

    responsive.css + lagt till materialize
    Lagt till media queries i responsive.css samt lagt till materialize

--- inc_header.php
+++ inc_header.php
@@ -21,12 +21,23 @@
 <html>
   <head>
     <!-- All the shared stuff here, specific things in each viewfile -->
+
+    <!-- Meta -->
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width,initial-scale=1">
+
+    <!-- Stylesheets -->
     <link rel="stylesheet" href="css/main.css" type="text/css">
     <link rel="stylesheet" href="css/responsive.css" type="text/css">
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+    <!-- Fonts -->
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700|Pirata+One' rel='stylesheet' type='text/css'>
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <!-- Scripts -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+    <script type="text/javascript" src="js/materialize.min.js"></script>
     <script type="text/javascript" src="js/main.js"></script>
     <script type="text/javascript" src="js/sha512.js"></script>
 
