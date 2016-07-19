a853c3226afce45dc8a98e846ebc5af1d0a492f3
Gustav <gubergma@gmail.com>
2016-07-19 17:05:03 +0200

    Lagt in bas för mobilmeny...

--- js/main.js
+++ js/main.js
@@ -53,4 +53,19 @@ Date.prototype.getDayOfWeek = function(){
   return d;
 }
 
+$(function() {
+  $('.toggle-nav').click(function() {
+      if ($('body').hasClass('show-nav')) {
+          $('body').removeClass('show-nav').addClass('hide-nav');
 
+          setTimeout(function() {
+              $('body').removeClass('hide-nav');
+          }, 500);
+
+      } else {
+          $('body').removeClass('hide-nav').addClass('show-nav');
+      }
+
+      return false;
+  });
+}
