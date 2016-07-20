39236bbabfa2984db49196506f50b23cbaac4c29
Gustav <gubergma@gmail.com>
2016-07-20 15:16:50 +0200

    Fixat schema

--- js/main.js
+++ js/main.js
@@ -31,39 +31,39 @@ function searchUsers(search) {
   });
 }
 
-// Date.prototype.getWeekNumber = function(){ //lägger till en funktion på Date som returnerar veckans nummer
-//   var d = new Date(+this);
-//   d.setHours(0,0,0);
-//   d.setDate(d.getDate()+4-(d.getDay()||7));
-//   return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
-// };
-// Date.prototype.addDays = function(days) { //Med den här funktionen kan man lägga till och dra ifrån dagar från ett datum
-//   var d = new Date(+this);
-//   d.setDate(d.getDate() + days);
-//   return d;
-// };
-// Date.prototype.getDayOfWeek = function(){
-//   var d = this.getDay();
-//   if (d === 0) {
-//     d = 6;
-//   } else {
-//     d -= 1;
-//   }
-//   return d;
-// }
+Date.prototype.getWeekNumber = function(){ //lägger till en funktion på Date som returnerar veckans nummer
+  var d = new Date(+this);
+  d.setHours(0,0,0);
+  d.setDate(d.getDate()+4-(d.getDay()||7));
+  return Math.ceil((((d-new Date(d.getFullYear(),0,1))/8.64e7)+1)/7);
+};
+Date.prototype.addDays = function(days) { //Med den här funktionen kan man lägga till och dra ifrån dagar från ett datum
+  var d = new Date(+this);
+  d.setDate(d.getDate() + days);
+  return d;
+};
+Date.prototype.getDayOfWeek = function(){
+  var d = this.getDay();
+  if (d === 0) {
+    d = 6;
+  } else {
+    d -= 1;
+  }
+  return d;
+}
 
 
 // Mobile menu
-(function() {
-  $('.mini-menu').on('click', function(e) {
-    alert('wat');
-    // $('html').toggleClass('is-mobile-nav-open');
+// (function() {
+//   $('.mini-menu').on('click', function(e) {
+//     alert('wat');
+//     // $('html').toggleClass('is-mobile-nav-open');
 
-    // e.stopPropagation();
-    // e.preventDefault();
-  });
+//     // e.stopPropagation();
+//     // e.preventDefault();
+//   });
 
-  // $('.is-mobile-nav-open .page').on('touchmove', function(e) {
-  //   e.preventDefault()
-  // });
-})();
+//   // $('.is-mobile-nav-open .page').on('touchmove', function(e) {
+//   //   e.preventDefault()
+//   // });
+// })();
