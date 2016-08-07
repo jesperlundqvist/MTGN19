58841a266891c16c4bf1d62bf2572bd01f09dfd4
Gubergma <gubergma@gmail.com>
2016-08-07 16:53:08 +0200

    Fixat ny sida så man kan lägga till event

--- adminpanel.php
+++ adminpanel.php
@@ -23,6 +23,7 @@
               <a class="admin-control" href="gallery_upload_images.php">Ladda upp bilder</a>
               <a class="admin-control" href="video_upload.php">Ladda upp video</a>
               <a class="admin-control" href="blandaren_upload.php">Ladda upp Bländare</a>
+              <a class="admin-control" href="event.php">Lägg till event</a>
             </div>
 
             <div id="news_input_container" class="news-top">
---
+++ event.php
@@ -0,0 +1,53 @@
+  <?php
+    //Load header
+    include_once ('inc_header.php');
+  ?>
+    <title>Lägg till nytt event</title>
+  </head>
+
+  <body>
+
+    <?php
+      //Load top content
+      include_once ('inc_top_content.php');
+    ?>
+
+    <div class="content-wrapper">
+      <!-- Content -->
+      <?php
+      if ($_SESSION['admin']) {
+
+        if ($_POST){
+          $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+
+          if ($stmt = $link->prepare("INSERT INTO event (name) VALUES (?)")) {
+          $stmt->bind_param('s', $name);
+          $stmt->execute();
+          if ($stmt->error) {
+            echo "Error: $stmt->error";
+          }
+
+          echo'<div class="center"><span style="color:green;">Event tillagt! </span></div>';
+  }
+        }
+      ?>
+        <h1>Lägg till nytt event</h1>
+        <form class="center" id="even  tform" action="event.php" method="post">
+          <input id="edit_title" name="name" placeholder="Namn" class="input_areas">
+          <input type="submit" value="Lägg till" class="button-primary">
+        </form>
+      <?php
+      }
+      else {
+        echo 'Du måste logga in som admin.';
+      }
+      ?>
+    </div>
+
+    <?php
+      //Load footer
+      include_once ('inc_footer.php');
+    ?>
+
+  </body>
+</html>
