99267d1e74049cca26179c8174c055e467c8eb48
Gubergma <gubergma@gmail.com>
2016-08-09 17:28:29 +0200

    småfix sidfot

--- inc_footer.php
+++ inc_footer.php
@@ -6,18 +6,19 @@
 <div class="footer-image">
 </div>
 <footer class="valign-wrapper">
+	<div class="constrainer">
 
 	<!-- Footer content -->
 	<!-- Medieteknik logo -->
-	<div class="medieteknik-wrapper valign">
-		<img class="medieteknik-logo" src="/images/medieteknik_logo_text.png"/>
+	<div class="footer-column valign">
+		<img class="footer-logo" src="/images/medieteknik_logo_text.png"/>
 	</div>
 	<svg class="divider-line-canvas">
 		<line class="divider-line" x1="0" y1="0" x2="0" y2="100%">
 	</svg>
 	<!-- Links -->
 	<!-- Instagram links -->
-	<div class="instagram-wrapper">
+	<div class="footer-column">
 		<img class="instagram-logo" src="/images/instagram.png"/>
 		<ul class="links-instagram">
 			<li class="links-element">
@@ -35,7 +36,7 @@
 		<line class="divider-line" x1="0" y1="0" x2="0" y2="100%">
 	</svg>
 	<!-- Snapchat links -->
-	<div class="snapchat-wrapper valign">
+	<div class="footer-column valign">
 		<img class="snapchat-logo" src="/images/snapchat-black.png"/>
 		<ul class="links-snapchat">
 			<li class="links-element">
@@ -49,47 +50,9 @@
 			</li>
 		</ul>
 	</div>
-	<svg class="divider-line-canvas">
-		<line class="divider-line" x1="0" y1="0" x2="0" y2="100%">
-	</svg>
-	<!-- Email links -->
-	<div class="email-wrapper valign">
-		<p class="email-title">Mail</p>
-		<ul class="links-email">
-			<li class="links-element">
-				<a href="mailto:oph@medieteknik.com" target="_top">oph@medieteknik.com</a>
-			</li>
-			<li class="links-element">
-				<a href="mailto:person1@medieteknik.com" target="_top">person1@medieteknik.com</a>
-			</li>
-			<li class="links-element">
-				<a href="mailto:person2@medieteknik.com" target="_top">person2@medieteknik.com</a>
-			</li>
-		</ul>
+
 	</div>
 
-	<!-- Credits -->
-	<!-- <div class="credits-wrapper">
-		<h3 class="credits-title">Creators</h3>
-		<ul class="credits-list">
-			<li class="credits-item">
-				<h4>Karl Andersson</h4>
-				<p>Roll</p>
-			</li>
-			<li class="credits-item">
-				<h4>Gustav Bergman</h4>
-				<p>Roll</p>
-			</li>
-			<li class="credits-item">
-				<h4>Johanna Bromark</h4>
-				<p>Roll</p>
-			</li>
-			<li class="credits-item">
-				<h4>Arvid Larsson</h4>
-				<p>Roll</p>
-			</li>
-		</ul>
-	</div> -->
 </footer>
 
 
