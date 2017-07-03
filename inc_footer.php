691f02170f17a631fdfe3688f193ff80e00ee58b
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2017-07-03 19:55:34 +0200

    Sponsordelen

--- inc_footer.php
+++ inc_footer.php
@@ -1,77 +1,75 @@
 <?php
-	//Footer template file
+    //Footer template file
 ?>
 
 
 <div class="footer-image">
 </div>
 <footer class="valign-wrapper">
-	<div class="constrainer">
 
-	<!-- Footer content -->
+    <div class="constrainer">
+    <!-- Footer content -->
 
-<!-- 	<svg class="divider-line-canvas">
-		<line class="divider-line" x1="0" y1="0" x2="0" y2="100%">
-	</svg> -->
-	<!-- Links -->
-	<!-- Instagram links -->
-	<div class="footer-column-right">
-		<img class="instagram-logo" src="/images/instagram.png"/>
-		<ul class="links-instagram">
-			<li class="links-element">
-				<a target="_blank" href="https://www.instagram.com/ofverphoseriet/">ofverphoseriet</a>
-			</li>
-			<li class="links-element">
-				<a target="_blank" href="https://www.instagram.com/kphgram/">kphgram</a>
-			</li>
-			<li class="links-element">
-				<a target="_blank" href="https://www.instagram.com/kennystarrfighter/" href="">kennystarrfighter</a>
-			</li>
-			<li class="links-element">
-				<a target="_blank" href="https://www.instagram.com/inphogram/" href="#">inphogram</a>
-			</li>
-			<?php
-			// <li class="links-element">
-			// 	<a target="_blank" href="https://www.instagram.com/lekogram/" href="#">lekogram</a>
-			// </li>
-			?>
-			<li class="links-element">
-				<a target="_blank" href="https://www.instagram.com/vraqueogram/" href="#">vraqueogram</a>
-			</li>
-		</ul>
-	</div>
-<!-- 	<svg class="divider-line-canvas">
-		<line class="divider-line" x1="0" y1="0" x2="0" y2="100%">
-	</svg> -->
-	<!-- Snapchat links -->
-	<div class="footer-column-right">
-		<img class="snapchat-logo" src="/images/snapchat-black.png"/>
-		<ul class="links-snapchat">
-			<li class="links-element">
-				<a href="#">kphsnap</a>
-			</li>
-			<li class="links-element">
-				<a href="#">arrsnap</a>
-			</li>
-			<li class="links-element">
-				<a href="#">inphosnap</a>
-			</li>
-		</ul>
-	</div>
+<!--     <svg class="divider-line-canvas">
+        <line class="divider-line" x1="0" y1="0" x2="0" y2="100%">
+    </svg>-->
+    <!-- Links -->
+    <!-- Instagram links -->
+    <div class="footer-column-right">
+        <img class="instagram-logo" src="/images/instagram.png"/>
+        <ul class="links-instagram">
+            <li class="links-element">
+                <a target="_blank" href="https://www.instagram.com/ofverphoseriet/">ofverphoseriet</a>
+            </li>
+            <li class="links-element">
+                <a target="_blank" href="https://www.instagram.com/kphgram/">kphgram</a>
+            </li>
+            <li class="links-element">
+                <a target="_blank" href="https://www.instagram.com/kennystarrfighter/" href="">kennystarrfighter</a>
+            </li>
+            <li class="links-element">
+                <a target="_blank" href="https://www.instagram.com/inphogram/" href="#">inphogram</a>
+            </li>
+            <?php
+            // <li class="links-element">
+            // 	<a target="_blank" href="https://www.instagram.com/lekogram/" href="#">lekogram</a>
+            // </li>
+            ?>
+            <li class="links-element">
+                <a target="_blank" href="https://www.instagram.com/vraqueogram/" href="#">vraqueogram</a>
+            </li>
+        </ul>
+    </div>
+<!-- 	   <svg class="divider-line-canvas">
+        <line class="divider-line" x1="0" y1="0" x2="0" y2="100%">
+    </svg> -->
+    <!-- Snapchat links -->
+    <div class="footer-column-right">
+        <img class="snapchat-logo" src="/images/snapchat-black.png"/>
+        <ul class="links-snapchat">
+            <li class="links-element">
+                <a href="#">kphsnap</a>
+            </li>
+            <li class="links-element">
+                <a href="#">arrsnap</a>
+            </li>
+            <li class="links-element">
+                <a href="#">inphosnap</a>
+            </li>
+        </ul>
+    </div>
 
-		<!-- Medieteknik logo -->
-	<div class="footer-column-left">
-		<div class="footer-logo-wrap"><img class="footer-logo" src="/design/logos/medie_logo.png"/></div>
-		<div class="footer-logo-wrap"><img class="footer-logo" src="/design/logos/nl_logo.png"/></div>
-		<div class="footer-logo-wrap"><img class="footer-logo comviq" src="/design/logos/comviq_logo.png"/></div>
-		<div class="footer-logo-wrap"><img class="footer-logo ericsson" src="/design/logos/ericsson_logo.png"/></div>
-		<div class="footer-logo-wrap"><a href="/newspage.php?id=17"><img class="footer-logo chas" src="/design/logos/chas_logo.png"/></a></div>
-		<div class="footer-logo-wrap"><img class="footer-logo" src="/design/logos/bontouch_logo.png"/></div>
-		<div class="footer-logo-wrap"><img class="footer-logo mtg" src="/design/logos/mtg_logo.png"/></div>
-	</div>
+        <!-- Medieteknik logo -->
+    <div class="footer-column-left">
+        <div class="footer-logo-wrap"><img class="footer-logo" src="/design/logos/medie_logo.png"/></div>
+        <div class="footer-logo-wrap"><img class="footer-logo" src="/design/logos/nl_logo.png"/></div>
+        <div class="footer-logo-wrap"><img class="footer-logo comviq" src="/design/logos/comviq_logo.png"/></div>
+        <div class="footer-logo-wrap"><img class="footer-logo ericsson" src="/design/logos/ericsson_logo.png"/></div>
+        <div class="footer-logo-wrap"><a href="/newspage.php?id=17"><img class="footer-logo chas" src="/design/logos/chas_logo.png"/></a></div>
+        <div class="footer-logo-wrap"><img class="footer-logo" src="/design/logos/bontouch_logo.png"/></div>
+        <div class="footer-logo-wrap"><img class="footer-logo mtg" src="/design/logos/mtg_logo.png"/></div>
+    </div>
 
-	</div>
+    </div>
 
 </footer>
-
-
