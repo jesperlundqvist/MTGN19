f5c1548be6870dc8799e27057fc773fc48164bad
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2017-07-20 22:13:21 +0200

    Stängde av autocomplete i inloggningsrutan

--- login.php
+++ login.php
@@ -15,7 +15,7 @@
       <div class="content-wrapper">
         <div class="login-wrap">
           <h1 class="login-title">Mottagningen</h1>
-            <input type="text" id="username" placeholder="Användarnamn"/>
+            <input type="text" id="username" placeholder="Användarnamn" autocorrect="off" />
             <br/>
             <input type="password" id="password" placeholder="Lösenord"/>
             <br/>
