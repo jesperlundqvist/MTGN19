eb279249683893b5913128baae0872ed9c91c402
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2017-07-21 19:42:43 +0200

    Nytt galleri!

---
+++ js/carousel.js
@@ -0,0 +1,178 @@
+$(document).on({
+    ajaxStart: function() {
+        $("#loader").addClass("loader-active");
+        $(".gallery").hide();
+    },
+    ajaxStop: function() {
+        $("#loader").removeClass("loader-active");
+        $(".gallery").show();
+    }
+});
+
+function getUrlFromElem(elem) {
+    var css = elem.css("background-image");
+    var img = css.replace(/(?:^url\("?.*images\/uploads\/gallery\/thumbs\/["']?|["']?"?\)$)/g, "");
+    img = "images/uploads/gallery/large/" + img;
+    return img;
+}
+
+function getVideoIdFromYouTubeThumb(elem) {
+    var css = elem.css("background-image");
+    var videoid = css.replace(/(?:^url\("?http:\/\/img.youtube.com\/vi\/["']?|["']?\/hqdefault.jpg"?\)$)/g, "");
+    return videoid;
+}
+
+function scrollToTarget(container, target) {
+    // Hur mycket utrymme tills den ska scrolla igen
+    var padding = 0;
+
+    // Relativa positioner till scrollbaren
+    var targetLeft = target.offset().left - container.offset().left;
+    var targetRight = targetLeft + target.width();
+
+    // Kolla ifall thumbnailen inte syns
+    if (targetLeft < padding || targetRight > container.width()-padding)
+    {
+        var scroll = 0;
+
+        // Åt vänster
+        if (targetLeft < 0)
+        {
+            scroll = targetLeft - padding;
+        }
+        // Åt höger
+        else if (targetRight > container.width())
+        {
+            scroll = targetRight - container.width() + padding;
+        }
+
+        scroll += container.scrollLeft();
+        container.scrollLeft(scroll);
+    }
+}
+
+function setLargeFromThumbElem(thumb) {
+    if (!thumb.is(current) && thumb.length != 0)
+    {
+        $("#gallery-ytplayer").attr("src", "");
+        $("#gallery-large-image").show();
+        $("#gallery-ytplayer").hide();
+
+        thumb.addClass("gallery-active-image");
+        if (current) {
+            current.removeClass("gallery-active-image");
+        }
+
+        if (thumb.hasClass("image"))
+        {
+            $("#gallery-large-image").attr('src', getUrlFromElem(thumb));
+        }
+        else if (thumb.hasClass("video"))
+        {
+            $("#gallery-ytplayer").attr("src", "https://www.youtube.com/embed/" + getVideoIdFromYouTubeThumb(thumb) + "?modestbranding=1&autoplay=1&rel=0&showinfo=0");
+            $("#gallery-ytplayer").show();
+            setAspectRatioOfPlayer();
+            $("#gallery-large-image").hide();
+        }
+
+        scrollToTarget($(".gallery-carousel"), thumb);
+
+        current = thumb;
+    }
+
+    if (thumb.length == 0) {
+        $("#gallery-large-image").attr('src', "");
+        $("#gallery-ytplayer").attr("src", "");
+        $("#gallery-large-image").hide();
+        $("#gallery-ytplayer").hide();
+    }
+}
+
+function previousImage() {
+    var prev = current.prevAll(".gallery-active-filter-type.gallery-active-filter-week.gallery-active-filter-event").eq(0);
+    if (prev.length && prev.is(".gallery-active-filter-type.gallery-active-filter-week.gallery-active-filter-event")) {
+        setLargeFromThumbElem(prev);
+    }
+}
+
+function nextImage() {
+    var next = current.nextAll(".gallery-active-filter-type.gallery-active-filter-week.gallery-active-filter-event").eq(0);
+    if (next.length) {
+        setLargeFromThumbElem(next);
+    }
+}
+
+function setAspectRatioOfPlayer() {
+    $("#gallery-ytplayer").height(($("#gallery-ytplayer").width()/16)*9);
+}
+
+function refreshFilter() {
+    $(".gallery-active-filter-type.gallery-active-filter-week.gallery-active-filter-event").show();
+    $(".gallery-carousel > div:not(.gallery-active-filter-type), .gallery-carousel > div:not(.gallery-active-filter-week), .gallery-carousel > div:not(.gallery-active-filter-event)").hide();
+    setLargeFromThumbElem($(".gallery-active-filter-type.gallery-active-filter-week.gallery-active-filter-event").eq(0));
+}
+
+$(function(){
+    current = 0;
+
+    setLargeFromThumbElem($(".gallery-carousel").children().eq(0));
+    setAspectRatioOfPlayer();
+
+    $(".gallery-carousel").children().click(function(e) {
+        setLargeFromThumbElem($(e.target));
+    });
+
+    $("#gallery-carousel-previous").click(previousImage);
+    $("#gallery-carousel-next").click(nextImage);
+
+    $(window).resize(setAspectRatioOfPlayer);
+
+    // Filter
+    $("#gallery-filter-type > li").click(function(e) {
+        var filter = $(e.target).data("filter");
+
+        if (filter == "all"){
+            $(".gallery-carousel > div").addClass("gallery-active-filter-type");
+        }
+        else if (filter == "image") {
+            $(".gallery-carousel > div").removeClass("gallery-active-filter-type");
+            $(".gallery-carousel > div.image").addClass("gallery-active-filter-type");
+        }
+        else if (filter == "video") {
+            $(".gallery-carousel > div").removeClass("gallery-active-filter-type");
+            $(".gallery-carousel > div.video").addClass("gallery-active-filter-type");
+        }
+
+        refreshFilter();
+    });
+
+    $("#gallery-filter-week > li").click(function(e) {
+        var week = $(e.target).data("filter");
+
+        if (week == "all")
+        {
+            $(".gallery-carousel > div").addClass("gallery-active-filter-week");
+        }
+        else {
+            $(".gallery-carousel > div[data-week='" + week + "']").addClass("gallery-active-filter-week");
+            $(".gallery-carousel > div:not([data-week='" + week + "'])").removeClass("gallery-active-filter-week");
+        }
+
+        refreshFilter();
+    });
+
+    $("#gallery-filter-event > li").click(function(e) {
+        var event = $(e.target).data("filter");
+
+        if (event == "all")
+        {
+            $(".gallery-carousel > div").addClass("gallery-active-filter-event");
+        }
+        else {
+            $(".gallery-carousel > div[data-event='" + event + "']").addClass("gallery-active-filter-event");
+            $(".gallery-carousel > div:not([data-event='" + event + "'])").removeClass("gallery-active-filter-event");
+        }
+
+        refreshFilter();
+    });
+})
--- media.php
+++ media.php
@@ -14,6 +14,9 @@
     <script type="text/javascript" src="js/jquery.fancybox.pack.js"></script>
     <script type="text/javascript" src="js/jquery.fancybox-media.js"></script>
     <script type="text/javascript" src="js/media.js"></script>
+    <script type="text/javascript" src="js/carousel.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/carousel.css">
   </head>
 
   <body>
@@ -26,24 +29,24 @@
     <div class="content-wrapper">
     <div id="popup_container"></div>
       <h1>Media</h1>
-
-      <?php
-      if ($admin) {
-        echo '<div id="deleteimagesbutton" class="deletestuff">Radera bilder</div>';
-        echo '<div id="deleteimagesbuttonVideo" class="deletestuff_video">Radera video</div>';
-      }
-      ?>
-
-      <div class="js-filter-show filter-show"><h3>Visa filter</h3></div>
+      <div id="loader">
+          <div class="rect1"></div>
+          <div class="rect2"></div>
+          <div class="rect3"></div>
+          <div class="rect4"></div>
+          <div class="rect5"></div>
+      </div>
+      <div class="gallery">
+          <div class="js-filter-show filter-show"><h3>Visa filter</h3></div>
       <div class="js-filter-hide filter-hide"><h3>Göm filter</h3></div>
       <div class="filters clearfix">
         <div class="filter-wrap">
           <h3>Typ</h3>
           <div class="button-group filters-button-group" data-filter-group="type">
-            <ul class="button-group-ul">
-              <li><a class="button is-checked" data-filter="">Visa alla</a></li>
-              <li><a class="button" data-filter=".filter-image">Bild</a></li>
-              <li><a class="button" data-filter=".filter-video">Video</a></li>
+            <ul class="button-group-ul" id="gallery-filter-type">
+              <li><a class="button is-checked" data-filter="all">Visa alla</a></li>
+              <li><a class="button" data-filter="image">Bild</a></li>
+              <li><a class="button" data-filter="video">Video</a></li>
             </ul>
           </div>
         </div>
@@ -51,14 +54,12 @@
         <div class="filter-wrap">
           <h3>Vecka</h3>
           <div class="button-group filters-button-group" data-filter-group="week">
-            <ul class="button-group-ul">
-              <li><a class="button is-checked" data-filter="">Visa alla</a>
-              <li><a class="button" data-filter=".filter-33">33</a></li>
-              <li><a class="button" data-filter=".filter-34">34</a></li>
-              <?php
-              // <li><a class="button" data-filter=".filter-35">35</a></li>
-              // <li><a class="button" data-filter=".filter-36">36</a></li>
-              ?>
+            <ul class="button-group-ul" id="gallery-filter-week">
+              <li><a class="button is-checked" data-filter="all">Visa alla</a>
+              <li><a class="button" data-filter="33">33</a></li>
+              <li><a class="button" data-filter="34">34</a></li>
+              <li><a class="button" data-filter="35">35</a></li>
+              <li><a class="button" data-filter="36">36</a></li>
             </ul>
           </div>
         </div>
@@ -70,47 +71,39 @@
         <div class="filter-wrap">
           <h3>Event</h3>
           <div class="button-group filters-button-group" data-filter-group="event">
-            <ul class="button-group-ul">
-              <li><a class="button is-checked" data-filter="">Visa&nbsp;alla</a></li>
+            <ul class="button-group-ul" id="gallery-filter-event">
+              <li><a class="button is-checked" data-filter="all">Visa&nbsp;alla</a></li>
               <?php
               while ($event = $result->fetch_object()) {
                 $name = $event->name;
-                echo'<li><a class="button" data-filter=".filter-'.$name.'">'.$name.'</a></li>';
+                echo'<li><a class="button" data-filter="'.$name.'">'.$name.'</a></li>';
               }
               ?>
             </ul>
           </div>
         </div>
 
-      </div> <!-- end filters -->
-      <div class="grid">
-
+      </div>
         <?php
         // Images
-        $query = "SELECT imagename, imagecreateddate, event, week FROM images ORDER BY imagecreateddate, uploaddate DESC";
+        $query = "SELECT * FROM ( SELECT imagename, uploaddate, event, week FROM images UNION SELECT videoid, uploaddate, event, week FROM videos) AS media ORDER BY uploaddate DESC";
         $result = execQuery($link, $query);
-        while ($image = $result->fetch_object()) {
-          echo'
-          <a class="fancybox-media" rel="gallery1" id="delete_' . $image->imagename . '" href="' . $imagelargepath . $image->imagename . '"><div class="filter-item filter-image filter-'.$image->week .' filter-'.$image->event .'" data-category="transition" style="background-image:url(' . $imagethumbpath . $image->imagename . ')">';
-          if ($admin) {
-              echo '<div class="delete deleteimage" id="delete_' . $image->imagename . '"><span class="ion-ios-trash-outline"></span> <span class="hide-on-mobile">DELETE </span>' . $image->imagename . '</div>';
-          }
-        echo'
-          </div></a>';
-        }
 
-        // Video
-        $query = "SELECT videoid, event, week FROM videos ORDER BY uploaddate DESC";
-        $result = execQuery($link, $query);
-        while ($video = $result->fetch_object()) {
-          echo'
-         <a class="fancybox-media" id="delete_' . $video->videoid . '" rel="gallery1" href="http://www.youtube.com/watch?v='.$video->videoid.'"><div class="filter-item filter-video filter-'.$video->week .' filter-'.$video->event .'" data-category="transition" style="background-image:url(http://img.youtube.com/vi/' . $video->videoid . '/hqdefault.jpg)"><img class="video-play-icon" src="/design/play_icon.png" />';
-         if ($admin) {
-              echo '<div class="delete_Video deletevideo" id="delete_' . $video->videoid . '"><span class="ion-ios-trash-outline"></span> <span class="hide-on-mobile">DELETE </span>' . $video->videoid . '</div>';
-          }
-          echo'
-          </div></a>';
+        echo "<div class='gallery-carousel' data-slideout-ignore>";
+        while ($media = $result->fetch_object()) {
+            // Bild
+            if (substr($media->imagename, -5, 1) == "." || substr($media->imagename, -4, 1) == ".")
+            {
+                echo "<div class='gallery-carousel-item image gallery-active-filter-type gallery-active-filter-week gallery-active-filter-event' data-week='" . $media->week . "' data-event='" . $media->event . "' ' style='background-image: url(" . $imagethumbpath . $media->imagename . ");'></div>";
+            }
+            // Video
+            else {
+                echo "<div class='gallery-carousel-item video gallery-active-filter-type gallery-active-filter-week gallery-active-filter-event' data-week='" . $media->week . "' data-event='" . $media->event . "' style='background-image: url(http://img.youtube.com/vi/" . $media->imagename . "/hqdefault.jpg);'><img class='video-play-icon' src='/design/play_icon.png' /></div>";
+            }
         }
+        echo "</div>";
+        echo "<div class='gallery-large'>";
+        echo "<div id='gallery-carousel-previous'>&#x2039;</div><img id='gallery-large-image'/><iframe id='gallery-ytplayer' type='text/html' src='' frameborder='0' allowfullscreen></iframe><div id='gallery-carousel-next'>&#x203a;</div></div>";
     ?>
     </div>
     </div>
