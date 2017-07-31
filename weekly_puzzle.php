7fce7f5eebc6d71998fc33b87d7d164b7d575e3b
Lucas Grönborg <lucas94gronborg@gmail.com>
2017-08-01 01:58:03 +0200

    Bytte ut fisken mot vindruvorna

--- weekly_puzzle.php
+++ weekly_puzzle.php
@@ -28,7 +28,7 @@
           </div>
           <div class="secret">
             <p>Code dare INPHO, sed in silentio. Vos autem nolo aliis audire loqui!</p>
-            <h3>LOREM IPSUM</h3>
+            <h1>LOREM IPSUM</h1>
           </div>
       </div>
     </div>
