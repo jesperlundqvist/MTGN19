0502ca96a975ac81f476b31329b0dd0e68c8a792
Lucas Grönborg <lucas94gronborg@gmail.com>
2017-08-13 23:31:48 +0200

    Veckans kluring fix

--- weekly_puzzle.php
+++ weekly_puzzle.php
@@ -17,7 +17,7 @@
       <div class="puzzle-wrap">
         <div class="puzzle">
           <h1 class=puzzle_title>Problema sabbati unum</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            <p>Lorem dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
               Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
               Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
@@ -26,10 +26,7 @@
             <button id="submit_answer_btn">Confirmo</button>
             <div id="error">Error, iterum conare!</div>
           </div>
-          <div class="secret">
-            <p>Code dare INPHO, sed in silentio. Vos autem nolo aliis audire loqui!</p>
-            <h1>LOREM IPSUM</h1>
-          </div>
+          <div class="secret"></div>
       </div>
     </div>
 
