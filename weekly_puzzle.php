3448f631e9704c886db044a902eb126d36e5c32b
Lucas Grönborg <lucas94gronborg@gmail.com>
2017-08-14 21:12:23 +0200

    Mindre text till kluringen

--- weekly_puzzle.php
+++ weekly_puzzle.php
@@ -17,10 +17,7 @@
       <div class="puzzle-wrap">
         <div class="puzzle">
           <h1 class=puzzle_title>Problema sabbati unum</h1>
-            <p>Lorem dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p>Lorem dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </br>
             <input type="text" id="puzzle-answer" placeholder="Responsum" autocorrect="off" />
             <button id="submit_answer_btn">Confirmo</button>
