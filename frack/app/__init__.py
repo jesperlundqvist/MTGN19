7c9774f378c9353c3e36a7cba2c5073c826f2e96
Fredrik Lundkvist <flundkvi@kth.se>
2018-05-16 17:02:57 +0200

    började på news-endpoint

--- frack/app/__init__.py
+++ frack/app/__init__.py
@@ -1,6 +1,7 @@
 #Den här filen används för att starta webservern
 from flask import Flask
 
-app = Flask(__name__) #Skapa ett flask-objekt
 
-from app import routes #importera routern
+app = Flask(__name__) #Skapa ett flask-objekt
+from app import news
+from app import routes #importera 
--- frack/app/routes.py
+++ frack/app/routes.py
@@ -1,6 +1,24 @@
 from app import app
+from flask import jsonify, request
+import json
+
+
+news_list = [
+    {"headline" : "n0llan ställer till det!", 
+    "text": "wow oh man wow n0llan fuckade upp!",
+    "author": "foppe", "tags":"cool"}
+]
 
 #Definiera olika URL-er och vad de leder till
 @app.route("/")
 def index():
     return "Du har fått igång flask!"
+
+@app.route("/news", methods = ["GET"])
+def get_news():
+    return jsonify(news_list)
+
+@app.route("/news", methods = ["POST", "PUT"])
+def add_news():
+    news_list.append(request.json)
+    return "Laddade upp artikel skriven av " + request.json["author"] +"\n"
