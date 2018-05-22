b51690720066a9da6d736761ac67881c69c41647
Fredrik Lundkvist <flundkvi@kth.se>
2018-05-22 16:32:44 +0200

    fixade permanent directory-variable för enklare returnering av filer (bilder, sior)

--- frack/app/routes.py
+++ frack/app/routes.py
@@ -1,17 +1,19 @@
+#Importera moduler
 from app.webapp import app as webapp
-from flask import jsonify, request
+from flask import jsonify, request, send_from_directory
 import json
 from app import news
+import os
 
 
-
+#definiera filsökväg till content
+static_dir = os.path.join(os.getcwd(), "static", "Schmeck")
 
 
 #Definiera olika URL-er och vad de leder till
 @webapp.route("/")
 def index():
-    return "Du har fått igång flask!"
-
+    return send_from_directory(static_dir, "index.html")
 @webapp.route("/news", methods = ["GET"])
 def get_news():
     return jsonify(news.get_news())
