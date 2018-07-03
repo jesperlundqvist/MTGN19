a1f61a4843963bb17b1f933bd498ef1e9e4548ba
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2018-07-03 20:35:36 +0200

    Grunden för bokdesignen

--- frack/app/__init__.py
+++ frack/app/__init__.py
@@ -8,11 +8,21 @@ db = SQLAlchemy(app)
 
 from app import routes
 from app.models.news import News
-from app.models.user import User
+from app.models.user import User, UserType, N0lleGroup
 db.create_all() #OBS UTAV BARA HELVETE denna ska inte finnas med i prod, den dumpar och skapar en ny databas
-testNews = News(headline="Hallå där!", author="INPHO", tags="", text="Här är det lite text!")
+
+inpho = UserType(name="INPHO")
+k = N0lleGroup(name="Magi-Karparna")
+
 adminUser = User(username="admin")
 adminUser.set_password("inpho")
+
+testNews = News(headline="Första inlägget", author="INPHO", tags="", text="Här är det lite text!")
+dag1News = News(headline="Välkommen nØllan!", author="ÖPH", tags="", text="Dag 1 är redan slut och vi hoppas att ni har haft en toppenstart tillsammans med oss!<br><br>Vi hoppas att ni ska trivas här hos oss på Medieteknik och att ni ser fram emot Mottagningen.<br><br>Under morgondagen vankas det gasque så passa på att komma till Gasquevettet där VRAQUE kommer lära er allt om hur man gasquear.<br><br>Tagga Välkomstgasque i morgon!")
 db.session.add(adminUser)
 db.session.add(testNews)
+db.session.add(dag1News)
+
+print(adminUser)
+
 db.session.commit()
--- frack/static/Schmeck/js/main.js
+++ frack/static/Schmeck/js/main.js
@@ -6,33 +6,66 @@ function renderTemplate(selector, url, data) {
         var template = Handlebars.compile(responseData);
 
         $(selector).html(template(data));
+        Frack.Router.updatePageLinks();
     }).fail(function(res) {
         console.error(res);
     });
 }
 
 $(document).ready(function() {
+    Handlebars.registerHelper('active', function(variable, value) {
+        if (variable == value) {
+            return new Handlebars.SafeString('active');
+        }
+        else {
+            return '';
+        }
+    });
+
     Frack.Router = new Navigo("http://localhost:5000");
 
     Frack.Router.on({
         '/': function() {
             Frack.News.GetAll().done(function(data) {
-                renderTemplate("#content", "/templates/home.html", {news: data});
+                renderTemplate("#content", "/templates/nyheter.html", {news: data});
+                renderTemplate("#sidebar", "/templates/sidebar.html", {currentPage: "nyheter"});
             });
         },
 
         '/nyhet/:id/': function(params, query) {
             Frack.News.GetById(params.id).done(function(data) {
                 renderTemplate("#content", "/templates/article.html", {article: data});
+                renderTemplate("#sidebar", "/templates/sidebar.html", {currentPage: "nyheter"});
             });
         },
+
+        '/schema': function() {
+            renderTemplate("#content", "/templates/schema.html");
+            renderTemplate("#sidebar", "/templates/sidebar.html", {currentPage: "schema"});
+        },
+
+        '/profiler': function() {
+            renderTemplate("#content", "/templates/profiler.html");
+            renderTemplate("#sidebar", "/templates/sidebar.html", {currentPage: "profiler"});
+        },
+
+        '/media': function() {
+            renderTemplate("#content", "/templates/media.html");
+            renderTemplate("#sidebar", "/templates/sidebar.html", {currentPage: "media"});
+        },
+
+        '/blandaren': function() {
+            renderTemplate("#content", "/templates/blandaren.html");
+            renderTemplate("#sidebar", "/templates/sidebar.html", {currentPage: "blandaren"});
+        },
+
         '/login': function() {
             renderTemplate("#page", "/templates/login.html");
         }
     });
 
     Frack.Router.notFound(function (query) {
-        renderTemplate("#content", "/templates/404.html", {});
+        renderTemplate("#page", "/templates/404.html");
     });
 
     Frack.Router.resolve();
