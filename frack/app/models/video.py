101836df84fc9a73ebe8371b42d35856c205e1db
Fredrik Lundkvist <fredrik.lundkvist@ericsson.com>
2018-07-04 10:37:14 +0200

    fixade video-klassen som hade försvunnit

---
+++ frack/app/models/video.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, Integer, String
+from app import app, db
+
+class Video(db.Model):
+    id = Column(Integer, primary_key = true)
+    uploaded_by = Column(String(64))
+    event = Column(String(256))
+    week = Column(Integer)
+    thumbnail = Column(String(256))
+
+    def as_dictionary():
+        vid_dict = {
+            "id": self.id,
+            "event": self.event,
+            "week":self.week,
+            "thumbnail":self.thumbnail
+        }
+
+        return vid_dict
