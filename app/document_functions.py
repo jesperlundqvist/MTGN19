df4d9e3d398522223552017f749e76473da1cba3
Fredrik Lundkvist <flundkvi@kth.se>
2018-07-22 22:26:13 +0200

    flyttade genereringen av thumbnails för PDF:er

--- app/document_functions.py
+++ app/document_functions.py
@@ -2,22 +2,31 @@ import os
 from sqlalchemy import desc, asc
 from app.models.blandaren import Document
 from app import db
+import sys
+import base64
 
 
 SAVE_FOLDER = os.path.join(os.getcwd(), "static", "blandaren")
 
 def upload_document(request):
+    print("function was called")
     files = request.files.getlist("files")
-    print(request.files)
+    
     if files is not None:
         for document in files:
             filename = document.filename
             filename = filename.replace(" ", "_")
             print(filename)
+            thumb_name = filename.split(".")[0] +".png"
+            thumbnail = request.form["thumbnail"]
+            with open(os.path.join(SAVE_FOLDER, thumb_name), "wb") as fh:
+                fh.write(base64.b64decode(thumbnail))
+
             document.save(os.path.join(SAVE_FOLDER, filename))
-            new_doc = Document(filename = filename)
+            new_doc = Document(filename = filename, thumbnail = thumb_name)
             db.session.add(new_doc)
     db.session.commit()
+    sys.stdout.flush()
     return True
 
 def get_documents():
