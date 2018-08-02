7d383abae0b5fc7c393fea23ef64b9882eef4762
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2018-08-03 00:11:04 +0200

    SCHEMA

--- static/js/main.js
+++ static/js/main.js
@@ -55,6 +55,53 @@ $(document).ready(function() {
         return new Handlebars.SafeString(str);
     });
 
+    Handlebars.registerHelper('toWeekDay', function(num) {
+        var str = "";
+
+        if (num == 6) {
+            str = "Söndag";
+        } else if (num == 0) {
+            str = "Måndag";
+        } else if (num == 1) {
+            str = "Tisdag";
+        } else if (num == 2) {
+            str = "Onsdag";
+        } else if (num == 3) {
+            str = "Torsdag";
+        } else if (num == 4) {
+            str = "Fredag";
+        } else if (num == 5) {
+            str = "Lördag";
+        }
+
+        return new Handlebars.SafeString(str);
+    });
+
+    function getWeekNumber(d) {
+        // Copy date so don't modify original
+        d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
+        // Set to nearest Thursday: current date + 4 - current day number
+        // Make Sunday's day number 7
+        d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay()||7));
+        // Get first day of year
+        var yearStart = new Date(Date.UTC(d.getUTCFullYear(),0,1));
+        // Calculate full weeks to nearest Thursday
+        var weekNo = Math.ceil(( ( (d - yearStart) / 86400000) + 1)/7);
+        // Return week number
+        return weekNo;
+    }
+
+    function getDateOfISOWeek(w, y) {
+        var simple = new Date(y, 0, 1 + (w - 1) * 7);
+        var dow = simple.getDay();
+        var ISOweekStart = simple;
+        if (dow <= 4)
+            ISOweekStart.setDate(simple.getDate() - simple.getDay() + 1);
+        else
+            ISOweekStart.setDate(simple.getDate() + 8 - simple.getDay());
+        return ISOweekStart;
+    }
+
     Frack.Router = new Navigo(window.location.origin);
 
     Frack.Router.hooks({
@@ -89,9 +136,114 @@ $(document).ready(function() {
             });
         },
 
-        '/schema': function() {
-            renderTemplate("#content", "/static/templates/schema.html");
-            renderTemplate("#sidebar", "/static/templates/sidebar.html", {currentPage: "schema", user: Frack.CurrentUser});
+        '/schema': function () {
+            var currentWeek = getWeekNumber(new Date());
+
+            if (currentWeek < 33) {
+                currentWeek = 33;
+            } else if (currentWeek > 36) {
+                currentWeek = 36;
+            }
+
+            Frack.Router.navigate('/schema/' + currentWeek);
+        },
+
+        '/schema/:week': function(params) {
+            var browserKey = 'AIzaSyC-0HQfVbXdEjF52aCm6rK9UMIrWglIskk';
+            var calendarid = "67o81suoslm4i02r6kr9vldek4@group.calendar.google.com"; // Mottagningen 2018
+
+            preloadTemplate("/static/templates/schema.html");
+            preloadTemplate("/static/templates/sidebar.html");
+
+            var weekToShow = parseInt(params.week);
+            var prevWeek = weekToShow - 1;
+            var nextWeek = weekToShow + 1;
+
+            if (prevWeek < 33) {
+                prevWeek = 0;
+            } else if (nextWeek > 36) {
+                nextWeek = 0;
+            }
+
+            var startQuery = getDateOfISOWeek(weekToShow, 2018);
+            var endQuery = new Date(startQuery);
+            endQuery.setUTCDate(endQuery.getUTCDate() + 7);
+
+            var currentDay = undefined;
+            var today = new Date();
+            if (today > startQuery && today < endQuery) {
+                currentDay = today.getUTCDay();
+
+                if (currentDay == 0) {
+                    currentDay = 6;
+                } else {
+                    currentDay--;
+                }
+            }
+
+            axios({
+                method: "get",
+                url: "https://www.googleapis.com/calendar/v3/calendars/" + calendarid + "/events",
+                params: {
+                    key: browserKey,
+                    orderBy: "startTime",
+                    singleEvents: true,
+                    timeMin: startQuery,
+                    timeMax: endQuery
+                }
+            }).then(function(res) {
+                var events = res.data.items;
+                var weeks = {};
+
+                var eventSizeMultiplier = 50;
+
+                events.forEach(function (ev, index) {
+                    var start = new Date(ev.start.dateTime);
+                    var end = new Date(ev.end.dateTime);
+
+                    var week = getWeekNumber(start);
+                    var day = start.getUTCDay();
+
+                    if (day == 0) {
+                        day = 6;
+                    } else {
+                        day--;
+                    }
+
+                    var diff = new Date(new Date(ev.end.dateTime) - new Date(ev.start.dateTime));
+                    ev["height"] = Math.round(diff.getUTCHours() * eventSizeMultiplier + (diff.getUTCMinutes()/60 * eventSizeMultiplier));
+
+                    ev["duration_str"] =
+                    start.toLocaleDateString("sv-SE", {
+                        weekday: 'long',
+                        year: 'numeric',
+                        month: 'long',
+                        day: 'numeric'
+                    }) + ", " +
+                    start.toLocaleTimeString("sv-SE", {"hour": "2-digit", "minute": "2-digit"}) + " - " + end.toLocaleTimeString("sv-SE", {"hour": "2-digit", "minute": "2-digit"});
+
+                    weeks[week] = weeks[week] || [undefined, undefined, undefined, undefined, undefined, undefined, undefined];
+                    weeks[week][day] = weeks[week][day] || [];
+                    weeks[week][day].push(ev);
+
+                    var i = weeks[week][day].length - 1;
+
+                    if (i == 0) {
+                        weeks[week][day][i]["topPosition"] = Math.round(start.getUTCHours() * eventSizeMultiplier + (start.getUTCMinutes()/60 * eventSizeMultiplier));
+                        weeks[week][day][i]["topPosition"] -= 6 * eventSizeMultiplier; // Börja varje dag kl 8
+                    }
+                    else {
+                        var prevEventEnd = new Date(weeks[week][day][i-1].end.dateTime);
+                        var diffToPrevious = new Date(new Date(ev.start.dateTime) - prevEventEnd);
+                        weeks[week][day][i]["topPosition"] = Math.round(diffToPrevious.getUTCHours() * eventSizeMultiplier + (diffToPrevious.getUTCMinutes()/60 * eventSizeMultiplier));
+                    }
+                });
+
+                renderTemplate("#content", "/static/templates/schema.html", {weeks: weeks, prevWeek: prevWeek, nextWeek: nextWeek, currentDay: currentDay});
+                renderTemplate("#sidebar", "/static/templates/sidebar.html", {currentPage: "schema", user: Frack.CurrentUser});
+            }).catch(function(error) {
+                console.error(error);
+            });
         },
 
         '/profiler': function() {
