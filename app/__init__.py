d9e0076aea2525dc5035ad9f6ef5bb4a838dffde
Fredrik Lundkvist <flundkvi@kth.se>
2018-08-08 20:01:03 +0200

    migrering?

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_pyfile('flask.cfg')
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
 from app import routes
 
@@ -53,27 +55,11 @@ if app.config["DEBUG"]:
     joppe.q2 = "Snö och kyla"
     joppe.q3 = "Joppus"
 
-    jespern0llan = User("jesper-n0llan", "Jesper-nØllan", "potatis", userTypeN0llan, magikarparna)
-    jespern0llan.profile_picture = "/static/images/profiles/jespern0llan.jpg"
-    jespern0llan.description = "Åh vad kul att börja på Medieteknik!"
-    jespern0llan.q1 = "Ja, absolut!"
-    jespern0llan.q2 = "Båt båt båt?"
-    jespern0llan.q3 = "Nej, aldrig!"
-
-    fredrikn0llan = User("fredrik-n0llan", "Fredrik-nØllan", "potatis", userTypeN0llan, krangligaKrabban)
-    fredrikn0llan.profile_picture = "/static/images/profiles/foppe.jpg"
-    olivern0llan = User("oliver-n0llan", "Oliver-nØllan", "potatis", userTypeN0llan, magikarparna)
-    olivern0llan.profile_picture = "/static/images/profiles/shin.jpg"
-    samueln0llan = User("samuel-n0llan", "Samuel-nØllan", "potatis", userTypeN0llan, hasten)
-    samueln0llan.profile_picture = "/static/images/profiles/kraft.jpg"
 
     testNews = News(headline="Första inlägget", author=joppe, tags="", text="Här är det lite text!")
     dag1News = News(headline="Välkommen nØllan!", author=adminUser, tags="", text="Dag 1 är redan slut och vi hoppas att ni har haft en toppenstart tillsammans med oss!<br><br>Vi hoppas att ni ska trivas här hos oss på Medieteknik och att ni ser fram emot Mottagningen.<br><br>Under morgondagen vankas det gasque så passa på att komma till Gasquevettet där VRAQUE kommer lära er allt om hur man gasquear.<br><br>Tagga Välkomstgasque i morgon!")
     db.session.add(adminUser)
-    db.session.add(jespern0llan)
-    db.session.add(fredrikn0llan)
-    db.session.add(olivern0llan)
-    db.session.add(samueln0llan)
+
     db.session.add(joppe)
     db.session.add(testNews)
     db.session.add(dag1News)
