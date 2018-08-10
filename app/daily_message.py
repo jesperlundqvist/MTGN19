be0854b29421cf742d241e5cbf328427f1ecca38
Jesper Lundqvist <jesper_lundqvist@icloud.com>
2018-08-10 13:54:36 +0200

    Hälsningar för vecka 1

--- app/daily_message.py
+++ app/daily_message.py
@@ -9,9 +9,17 @@ def get_daily_messange_json():
     message = ""
 
     if month == 8 and day == 13:
-        message = "Välkommen till Medieteknik och KTH, nØllan! Upprop, lunch, kontrollskrivning osv."
+        message = "Välkommen till Medieteknik och KTH, nØllan! Efter uppropet idag kommer VRAQUE visa er runt på campus och vi kommer även bjuda på lunch senare! På eftermiddagen är det en kontrollskrivning i matte, men oroa er inte, den är bara för att KTH vill ha statistik, det är inget allvarligt!"
     elif month == 8 and day == 14:
         message = "Ikväll är det dags för Välkomstgasquen, er första gasque med oss! Kom ihåg att gå på Gasquevettet kl 13 där VRAQUE kommer lära er hur man gasquear!"
+    elif month == 8 and day == 15:
+        message = "Idag har företaget Cygni en lunchföreläsning där de bjuder på lunch, så lämna matlådan hemma! Senare är det är det dags för en av Medias äldsta traditioner, Ö&B!"
+    elif month == 8 and day == 16:
+        message = "På eftermiddagen idag kommer ni få lite viktig information om sektionen och utbildningen. På kvällen är det Phångarna på Phortet, det kommer bli kul!"
+    elif month == 8 and day == 17:
+        message = "Kl 9 idag kommer ni få träffa och lära känna vårt klubbmästeri, MKM! Idag kan ni också lämna matlådan hemma då Sveriges Ingenjörer bjuder på lunchföreläsning. På eftermiddagen tar vi det lungt med lite Phika på philt!"
+    elif month == 8 and day == 18:
+        message = "Idag är det dags för Neverland-gasquen! Vi träffas i META kl 15!"
     elif month == 9 and day == 8:
         message = "nØllan vill inte missa nØg."
     elif month >= 9 and day >= 9:
