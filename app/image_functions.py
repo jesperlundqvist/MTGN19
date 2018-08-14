1b25e9eed4d5be0019e04f473e85f6303febcebd
Fredrik Lundkvist <flundkvi@kth.se>
2018-08-14 11:11:43 +0200

    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- app/image_functions.py
+++ app/image_functions.py
@@ -137,7 +137,7 @@ def get_events():
 def handle_video(video_link):
     video_id = video_link.split("v=")[1]
     print(video_id)
-    embed_link = "https://youtube.com/embed/" + video_id
+    embed_link = "youtube.com/embed/" + video_id
     thumbnail = "https://img.youtube.com/vi/" + video_id + "/maxresdefault.jpg"
 
     return embed_link, thumbnail
