ed91253221449679f2fea1b14cdea037f4654076
mrich <mrich@kth.se>
2019-07-15 16:11:16 +0200

    Updated admin and blandaren

--- schmeck/src/Admin/Admin.jsx
+++ schmeck/src/Admin/Admin.jsx
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
-
+import { Link } from "react-router-dom";
+import "./Admin.css"
 class Admin extends Component {
   state = {};
   render() {
     return (
       <div className="page">
         <h1 className="view_header">Admin</h1>
-        <div>
-          <h3>Frågan är om vi ska ha kvar det här eller göra om?</h3>
+        <div className="admin_container">
+
+          <Link className="links long_link" to="/admin/inlagg">Hantera inlägg</Link>
+
+          <Link className="links long_link" to="/admin/media">Hantera media</Link>
+
+          <Link className="links long_link" to="/admin/blandaren">Ladda upp ny bländaren</Link>
+
+          <Link className="links long_link" to="/admin/anvandare">Skapa/hantera ny användare</Link>
+
+          <Link className="links long_link" to="/admin/n0llegrupper">Hantera n0llegrupper</Link>
+          <Link className="links long_link" to="/admin/anvandartyper">Hantera användartyper</Link>
         </div>
       </div>
     );
