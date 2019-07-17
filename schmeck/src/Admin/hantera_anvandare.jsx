48fc2b9aa22d3d66f7a1708896f8341e3d5b18be
mrich <mrich@kth.se>
2019-07-17 13:20:56 +0200

    uppdaterad admin del
    man kan nu skapa användare men hantera användare är inte klar än!

--- schmeck/src/Admin/hantera_anvandare.jsx
+++ schmeck/src/Admin/hantera_anvandare.jsx
@@ -1,13 +1,270 @@
 import React, { Component } from "react";
+import Frack from "./../Frack";
 import "./Admin.css"
 class Anvandare extends Component {
-    state = {};
+    state = {
+        types: [],
+        n0llegroup: [],
+        users: [],
+        currentUser: "",
+    };
+
+    componentDidMount() {
+        Frack.UserType.GetAll().then(res => {
+            console.log("usertypes: ", res.data)
+            this.setState({ types: res.data });
+        });
+        Frack.N0lleGroup.GetAll().then(res => {
+            console.log("n0llegroups: ", res.data)
+            this.setState({ n0llegroup: res.data });
+        });
+        Frack.User.GetAll().then(res => {
+            console.log("Users: ", res.data)
+            this.setState({ users: res.data });
+        });
+    }
+
+    changeHandler(e) {
+        
+        console.log(e.target.id, ": ", e.target.value)
+        //this.setState({ });
+    }
+
+    submitNewUser(e) {
+        e.preventDefault()
+        let name = e.target.username.value
+
+
+
+        if (e.target.usertype.value == "1") {
+            name += "-nØllan";
+        }
+
+        console.log("Username: ", name.toLowerCase())
+        console.log("Usertype: ", e.target.usertype.value)
+        console.log("n0llegroup: ", e.target.n0llegroup.value)
+
+        var data = {
+            name: name,
+            username: name.toLowerCase(),
+            password: "potatis",
+            type_id: e.target.usertype.value
+        };
+        if (e.target.n0llegroup.value != -1) {
+            data["n0llegroup_id"] = e.target.n0llegroup.value;
+        }
+        Frack.User.New(data).then(function (res) {
+            console.log("Skapat ny användare. ", res.data);
+            //$("#createdUserAlert").show();
+            //$("#createdUserAlert").text("Skapade användare " + username + "!");
+            //$("#input_name").val("");
+        });
+    }
+
+
+    saveAllUsers() {
+        console.log("SaveAllUsers")
+        this.state.users.map((user) => {
+            this.saveUser(user)
+        })
+    }
+
+    saveUser(user) {
+        console.log("SaveUser: ")
+        console.log(user)
+
+        //$("#input_alert_success_" + id).hide();
+
+        /*
+        var data = {
+            username: user.username,
+            name: user.name,
+            type_id: ,
+            admin: $("#input_admin_" + id).is(':checked'),
+            hidden: $("#input_hidden_" + id).is(':checked')
+        };
+
+        if ($("#input_n0llegroup_" + id).val() != -1)
+        {
+            data["n0llegroup_id"] = $("#input_n0llegroup_" + id).val();
+        }
+        else
+        {
+            if ($("#input_type_" + id + " option:selected").text() == "nØllan")
+            {
+                if (!confirm("nØllan \"" + data.username + "\" har ingen nØllegrupp! Är du säker på att det ska vara så?"))
+                {
+                    return;
+                }
+            }
+        }
+
+        var profilePicutureRequest = [];
+
+        if ($("#input_picture_" + id).prop('files').length > 0)
+        {
+            var file = $("#input_picture_" + id).prop('files')[0];
+            var formData = new FormData();
+            formData.append("image", file);
+
+            profilePicutureRequest.push(axios({
+                method: "post",
+                url: "/api/upload_profile_picture/" + data.username,
+                data: formData,
+                auth: {
+                    username: sessionStorage.authToken,
+                    password: ""
+                }
+            }).catch(function(error) {
+                console.error(error);
+            }));
+        }
+
+        axios.all(profilePicutureRequest).then(function(responses) {
+            if (responses.length > 0) {
+                //$("#input_picture_" + id).val("");
+                data["profile_picture"] = responses[0].data.url;
+            }
+
+            Frack.User.Update(id, data).then(function (res) {
+                //$("#input_alert_success_" + id).show();
+                //$("#input_alert_success_" + id).text("Ändringar är sparade!");
+            });
+        });*/
+    }
+
+    removeUser(id) {
+        console.log("Remove user: ")
+        console.log(id)
+        //let id = e.target.value;
+        /*if (confirm("Är du säker på att du vill ta bort användaren?"))
+        {
+            Frack.User.Delete(id).then(function(res) {
+                /*$("#user_" + id).remove();
+            });
+        }*/
+    }
+
+    userResetPassword(id) {
+        console.log("reset password: ")
+        console.log(id)
+        //let id = e.target.value;
+        var newPassword = "lösenord1";
+
+        Frack.User.Update(id, { password: newPassword }).then(function (res) {
+            /*$("#input_alert_success_" + id).show();
+            $("#input_alert_success_" + id).text("Nytt lösenord är \"" + newPassword + "\"!");*/
+        });
+    }
+
     render() {
+        //Form(HTML) för att skapa användare
+        var create_user = <div className="admin_block">
+            <h1 className="view_header">Skapa användare</h1>
+            <form className="form-group" onSubmit={this.submitNewUser}>
+                <label className="form_label">Namn: </label>
+                <input type="text" className="form-control" placeholder="Namn" name="username" />
+                <br />
+                <label className="form_label">Användartyp: </label>
+                <select className="form-control" name="usertype">
+
+                    {this.state.types.map((type) => {
+                        return (<option key={type.id} value={type.id}>{type.name}</option>)
+                    })}
+
+                </select>
+                <br />
+                <label className="form_label">nØllegrupp: </label>
+                <select className="form-control" name="n0llegroup">
+                    <option value="-1">Ingen</option>
+                    {this.state.n0llegroup.map((group) => {
+                        return (<option key={group.id} value={group.id}>{group.name}</option>)
+                    })}
+
+                </select>
+                <br />
+                <input type="submit" className="btn btn-primary" value="Skapa ny användare" />
+            </form>
+        </div>
+
+        //Form(HTML) för att uppdatera användare
+        var update_user =
+            <div className="admin_block">
+                <h1 className="view_header">Hantera användare</h1>
+                <ul class="list-group" id="user_list">
+
+                    {this.state.users.map((user) => {
+                        return (
+                            
+                                <li class="list-group-item flex-column align-items-start" id={user.id}>
+                                    <form onSubmit={() => this.saveUser(user)}>
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <label class="form_label">Användarnamn: </label>
+                                        <input type="text" id={`input_username_${user.id}`} class="inline_user_edit_input" defaultValue={user.username} onBlur={this.changeHandler}/>
+                                    </div>
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <label class="form_label">Namn: </label>
+                                        <input type="text" id={`input_name${user.id}`} class="inline_user_edit_input" defaultValue={user.name} onBlur={this.changeHandler}/>
+                                    </div>
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <label class="form_label">Typ: </label>
+                                        <select id={`input_type_${user.id}`}>
+                                            {this.state.types.map((type) => {
+                                                return (<option key={type.id} value={type.id}>{type.name}</option>)
+                                            })}
+                                        </select>
+                                    </div>
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <label class="form_label">nØllegrupp: </label>
+                                        <select id={`input_n0llegroup_${user.id}`}>
+                                            <option value="-1">Ingen</option>
+                                            {this.state.n0llegroup.map((group) => {
+                                                return (<option key={group.id} value={group.id}>{group.name}</option>)
+                                            })}
+                                        </select>
+                                    </div>
+
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <label class="form_label">Admin: </label>
+                                        <input id={`input_admin_${user.id}`} type="checkbox" />
+                                    </div>
+
+                                    <div class="d-flex w-100 justify-content-between">
+                                        <label class="form_label">Dold: </label>
+                                        <input id={`input_hidden_${user.id}`} type="checkbox" />
+                                    </div>
+
+                                    <div>
+                                        <label class="form_label">Ny profilbild: </label>
+                                        <br />
+                                        <input type="file" id={`input_picture_${user.id}`} />
+                                    </div>
+
+                                    <br />
+
+                                    <div class="d-flex w-100 justify-content-between">
+                                    <input type="submit" class="btn btn-primary btn-sm" value={user.id} onClick={() => this.saveUser(user)}>Spara</input>
+                                    <button type="button" class="btn btn-secondary btn-sm" value={user.id} onClick={() => this.userResetPassword(user)}>Återställ lösenord</button>
+                                    <button type="button" class="btn btn-secondary btn-sm" value={user.id} onClick={() => this.removeUser(user)}>Ta bort</button>
+                                </div>
+
+                                </form>
+                                <br />
+                                <div id={`input_alert_success_${user.id}`} class="alert alert-success" role="alert" />
+                            </li>
+                        )
+                    })
+                    }
+                </ul>
+                <br />
+                <button type="button" class="btn btn-primary" onClick={this.saveAllUsers}>Spara alla ändringar</button>
+            </div>
+
         return (
             <div className="page">
-                <h1 className="view_header">Skapa användare</h1>
-                <h1 className="view_header">Hantera användare</h1>
-
+                {create_user}
+                {update_user
+                }
             </div>
         );
     }
--- schmeck/src/Admin/hantera_blandaren.jsx
+++ schmeck/src/Admin/hantera_blandaren.jsx
@@ -2,11 +2,141 @@ import React, { Component } from "react";
 import "./Admin.css"
 class HanteraBlandaren extends Component {
     state = {};
+
+
+    createNewBlandaren(data) {
+        /*Frack.Blandaren.New(data)*/
+    }
+
+    deleteBlandaren(data) {
+        /*Frack.Blandaren.Delete(data)*/
+    }
+
+
+    uploadMedia(e) {
+        e.preventDefault();
+        console.log(e)
+        // funktion för uppladning av bilder till servern
+        var outputData = [];
+        /*var fileSelect = document.getElementById("files");
+        var files = fileSelect.files;
+        var week = $("#week").val(); // veckan bilden togs
+        var event = $("#event").val(); // eventet bilden togs under
+        var links = $("#videos").val();
+        var linkList = links.split(",");
+        linkList = linkList.filter(v => v != "");
+        var form_data = new FormData();
+
+        for (var i = 0; i < files.length; i++) {
+            var file = files[i];
+            form_data.append("files", file, file.name)
+        }
+        form_data.append("week", week);
+        form_data.append("event", event);
+
+
+        for (var j = 0; j < linkList.length; j++) {
+            var link = linkList[j];
+            form_data.append("videos", link);
+        }
+
+        $.ajax({
+            url: "/api/media/",
+            type: "POST",
+            data: form_data,
+            contentType: false,
+            processData: false,
+            success: function () {
+                window.location.href = "/media";
+            }
+        })*/
+    };
+
+    uploadDocument = function () {
+        /*var fileSelect = document.getElementById("files");
+        var files = fileSelect.files;
+
+
+        var form_data = new FormData();
+
+        for (var j = 0; j < files.length; j++) {
+            var file = files[j];
+            form_data.append("files", file, file.name);
+        }
+        $(".lightbox").css("display", "flex")
+            .css("justify-content", "center")
+            .css("align-items", "center");
+
+        generateThumbNail(files, form_data)*/
+
+
+
+
+    };
+    generateThumbNail(fileList, form_data) {
+        /*
+        PDFJS.workerSrc = "/static/js/pdf.worker.js";
+        var pdfWorker = new PDFJS.PDFWorker();
+        var DOC_URL = "/static/blandaren/";
+        var title = $("#blandar_title").val();
+        $(".upload-status").text("Skapar thumbnail..")
+        for (var i = 0; i < fileList.length; i++) {
+            var docObj = fileList[i];
+            var fileReader = new FileReader();
+            pdf_file = docObj.filename;
+            fileReader.readAsDataURL(docObj)
+            fileReader.onload = (event) => {
+                var typedArray = new Uint8Array(event.target.result);
+                PDFJS.getDocument(event.target.result).then(function (pdf) {
+                    pdf.getPage(1).then(function (page) {
+                        var viewport = page.getViewport(0.5);
+                        var canvas = document.createElement('canvas');
+                        var ctx = canvas.getContext('2d');
+                        canvas.height = viewport.height;
+                        canvas.width = viewport.width;
+                        var renderContext = {
+                            canvasContext: ctx,
+                            viewport: viewport
+                        };
+                        page.render(renderContext).promise.then(() => {
+                            // Draw behind current content
+                            ctx.globalCompositeOperation = "destination-over";
+                            // Set background color
+                            ctx.fillStyle = "#fff";
+                            // Draw on entire canvas
+                            ctx.fillRect(0, 0, canvas.width, canvas.height);
+                            // Create image from canvas
+                            var img_src = canvas.toDataURL("image/png");
+                            img_src = img_src.split(",")[1];
+                            form_data.append("thumbnail", img_src)
+                            form_data.append("title", title)
+                            $(".upload-status").empty();
+                            $(".upload-status").text("Laddar upp..")
+                            $.ajax({
+                                url: "/api/blandaren",
+                                type: "POST",
+                                data: form_data,
+                                contentType: false,
+                                processData: false,
+                                success: function () {
+                                    window.location.href = "/blandaren";
+                                }
+                            })
+                        })
+                    })
+                })
+            }
+        }
+
+        */
+    };
+
+
     render() {
         return (
             <div className="page">
                 <h1 className="view_header">Ladda upp bländaren</h1>
-                
+
 
             </div>
         );
